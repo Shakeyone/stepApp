038024bcd16a6c80d4e60789fdf5c13d8e07a1c5
Chandler <chandler.ryan@live.com>
2019-05-23 08:21:20 -0500

    opps forgot to save one

---
+++ resources/views/friends/create.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.layout')
+
+@section('title', 'Add a friend')
+
+@section('header', 'Create a new step entry.')
+
+@section('content')
+
+    <div class="col-sm-6 offset-sm-3">
+
+        <form action="/steps" method="POST">
+            
+            @csrf
+            
+            <div class="input-group mb-3">
+                <input type="number" name="stepTotal" id="stepTotal" placeholder="Total Steps" 
+                    class="form-control {{$errors->has('stepTotal') ? 'border-danger' : ''}}"
+                    @if (old('stepTotal')>0) value="{{old('stepTotal')}}"@endif>
+                <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+            </div>
+    
+            <div class="input-group mb-3">
+                <input type="date" name="stepTotalDate" id="stepTotalDate"
+                class="form-control" @if (old('stepTotalDate')>0) value="{{old('stepTotalDate')}}"@endif>
+            </div>
+    
+            <div class="input-group mb-3">
+                <button type="submit" class="btn btn-success">Add Steps</button>
+            </div>
+    
+            @if($errors->any())
+                <div class="validation">
+                    <ul class="list-group">
+                        @foreach ($errors->all() as $error)
+                            <li class="list-group-item list-group-item-danger">{{ $error }}</li> 
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+    
+        </form>
+
+    </div>
+
+@endsection
